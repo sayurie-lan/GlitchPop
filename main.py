507b341a0f479fd10d02839bb7f1eb940b7777f4
unknown <alikhanullly@gmail.com>
2021-04-17 14:18:32 +0600

    Hello GitHub

---
+++ main.py
@@ -0,0 +1,44 @@
+# print("hello world")
+# print("ALibi chort")
+
+# data types
+
+# string
+# a = 'asdasd'
+# b = "asdasdasd"
+# c = '''asdasdasd'''
+
+# int
+# a = 1123
+
+# # float
+# a = 23.123
+
+# a1 = [1,2,3, '223']
+
+# d = {
+#   'asd': 123,
+#   '23': 233
+# }
+
+# # boolean
+# a = True
+# a = False
+
+# print(type(d))
+
+# a = '123'      ПРЕОБРАЗОВАТЬ ПЕРЕМЕННЫЕ ТИПЫ
+# a = str(a)
+# print(a)
+# print(type(a))
+
+# x = range(5, 6)
+# print(x)
+
+a = 1
+b = "1"
+c = 1.14
+
+d = a + float(b) + c
+
+print(round(d, 3))
